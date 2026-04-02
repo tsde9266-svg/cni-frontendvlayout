4bf8b4313172e64e0d3122353281329e5459710f
unknown <tsde9266@gmail.com>
2026-04-02 15:22:46 +0100

    first commit

--- src/app/(public)/search/page.tsx
+++ src/app/(public)/search/page.tsx
@@ -1,4 +1,7 @@
 'use client';
+
+export const dynamic = 'force-dynamic';
+
 import { useState } from 'react';
 import { useSearchParams } from 'next/navigation';
 import { useQuery } from '@tanstack/react-query';
